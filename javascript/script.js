81ee48021179e968be0b0ed6356878fe2f43dff5
Wonki Hwang <meteor_swarm@hotmail.com>
2019-08-27 12:44:15 -0400

    First Commit

---
+++ javascript/script.js
@@ -0,0 +1,49 @@
+$(document).ready(function () {
+    window.onscroll = function () { myFunction() };
+
+    var navbar = document.getElementById("navbar");
+    var sticky = navbar.offsetTop;
+
+    function myFunction() {
+        if (window.pageYOffset >= sticky) {
+            navbar.classList.add("sticky")
+        } else {
+            navbar.classList.remove("sticky");
+        }
+    }
+
+    $('.dropdown-menu a.dropdownToggle').on('click', function (e) {
+        if (!$(this).next().hasClass('show')) {
+            $(this).parents('.dropdown-menu').first().find('.show').removeClass("show");
+        }
+        var $subMenu = $(this).next(".dropdown-menu");
+        $subMenu.toggleClass('show');
+
+        $(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function (e) {
+            $('.dropdown-submenu .show').removeClass("show");
+        });
+
+        return false;
+    });
+
+    $('#appointment_phone').on('input', function () {
+        var number = $(this).val().replace(/[^\d]/g, '')
+        if (number.length == 7) {
+            number = number.replace(/(\d{3})(\d{4})/, "$1-$2");
+        } else if (number.length == 10) {
+            number = number.replace(/(\d{3})(\d{3})(\d{4})/, "$1-$2-$3");
+        }
+        $(this).val(number)
+    });
+
+    $(".phone_num").keypress(function (e) {
+        if (e.which != 8 && e.which != 0 && String.fromCharCode(e.which) != '-' && (e.which < 48 || e.which > 57)) {
+            return false;
+        }
+    });
+
+    $('.btn-tiffany').on('click', function() {
+        history.back();
+        return false;
+    })
+});
